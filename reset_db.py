0f4fafaf3480a1b0a64e44dc7d314d98c949d6bc
prince mehta <154504461+CreatorguyS@users.noreply.github.com>
2026-04-15 23:57:15 +0530

    working version

--- reset_db.py
+++
@@ -1,9 +0,0 @@
-from database import Base, engine
-
-print("Dropping old table...")
-Base.metadata.drop_all(bind=engine)
-
-print("Creating new table with 'content' column...")
-Base.metadata.create_all(bind=engine)
-
-print("Done! Database is synced.")
